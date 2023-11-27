fb22ab4dbd0e7268d461058096c7ae7a6fcaeb00
NataliaBogliotti <nataliabogliotti4@gmail.com>
2023-11-26 21:52:42 -0300

    Cambio en el src de las imagenes

--- src/components/CarouselHome/CarouselHome.tsx
+++ src/components/CarouselHome/CarouselHome.tsx
@@ -9,7 +9,7 @@ const CarouselHome = () => {
         <Carousel.Item>
           <img className='b block  w-100'
             style={{ maxHeight: "500px", objectFit: 'cover' }}
-            src="public/assets/images/img 2.jpg" alt="img1" />
+            src="/assets/images/img 2.jpg" alt="img1" />
           <Carousel.Caption>
             <h3 style={{fontSize:"40px", fontStyle:"bold"}}>Bienvenido al mundo de la productividad</h3>
             <p style={{fontSize:"20px"}}>Organiza tu día con un eficiente listado de tareas.</p>
@@ -19,13 +19,13 @@ const CarouselHome = () => {
         <Carousel.Item>
           <img className='b block  w-100'
             style={{ maxHeight: "500px", objectFit: 'cover' }}
-            src="public/assets/images/img 1.jpg" alt="img2" />
+            src="/assets/images/img 1.jpg" alt="img2" />
         </Carousel.Item>
 
         <Carousel.Item>
           <img className='b block  w-100'
             style={{ maxHeight: "500px", objectFit: 'cover' }}
-            src="public/assets/images/img 3.jpeg" alt="img3" />
+            src="/assets/images/img 3.jpeg" alt="img3" />
           <Carousel.Caption style={{fontSize:"30px", fontStyle:"bold"}}>
             <p>Utiliza aplicaciones o agendas para gestionar tu lista de tareas</p>
           </Carousel.Caption>
